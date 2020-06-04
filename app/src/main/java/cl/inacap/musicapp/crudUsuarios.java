46a2b6ff4874a8070836047591aa5a513e9609c9
Ignacio Seco <ignacio.seco@inacapmail.cl>
2020-06-04 04:17:56 -0400

    Unión de Partes

--- app/src/main/java/cl/inacap/musicapp/crudUsuarios.java
+++ app/src/main/java/cl/inacap/musicapp/crudUsuarios.java
@@ -6,11 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class crudUsuarios extends AppCompatActivity {
 
@@ -25,7 +37,9 @@ public class crudUsuarios extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crud_usuarios);
-
+        String[] operaciones = {"Administrador","Profesor","Usuario"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,R.layout.support_simple_spinner_dropdown_item, operaciones);
+        etTipoUsu.setAdapter(adapter);
         btnOpenAnim = AnimationUtils.loadAnimation(crudUsuarios.this,R.anim.fab_open);
         btnCloseAnim = AnimationUtils.loadAnimation(crudUsuarios.this,R.anim.fab_close);
 
@@ -70,5 +84,43 @@ public class crudUsuarios extends AppCompatActivity {
                 }
             }
         });
+
+        fbtnAgregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ejecutar("http://localhost/phpmyadmin/index.php");
+            }
+        });
+    }
+    private void ejecutar(String URL){
+        StringRequest stringRequest= new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Toast.makeText(getApplicationContext(), "OPERACIÓN EXITOSA", Toast.LENGTH_LONG).show();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String,String> parametros = new HashMap<String,String>();
+                parametros.put("Run",etRut.getText().toString());
+                parametros.put("Nombre",etNombre.getText().toString());
+                parametros.put("ApPaterno",etApePat.getText().toString());
+                parametros.put("ApMaterno",etApeMat.getText().toString());
+                parametros.put("Direccion",etDirec.getText().toString());
+                parametros.put("Email",etMail.getText().toString());
+                parametros.put("Nacimiento",etFecNac.getText().toString());
+                parametros.put("user",etUsu.getText().toString());
+                parametros.put("pass",etPass.getText().toString());
+                parametros.put("Perfil",etTipoUsu.getSelectedItem().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 }
